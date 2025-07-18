492779da0c89f64e88a253c874f62c4ebf08ce88
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-19 01:10:17 +0600

    Add functionTypes.js and other files

--- basic.js
+++ basic.js
@@ -13,3 +13,29 @@
 // printDetails("Bakir", "25", "Cumilla");
 
 
+// function add(x , y){
+//       const total = x + y;
+//       return total;
+// }
+// console.log(add(4,5));
+// //  const z = add(5 , 4);
+// //  console.log(z);
+
+
+
+//return kno korbo;(return korar age )
+function givenNumber (){
+      console.log("inside function" ,5);
+}
+const x = givenNumber();
+console.log("outside function" , x);
+
+//return korar por ;
+function givenNum (){
+      console.log("inside function", 5);
+return 5;
+}
+const y = givenNum();
+console.log("out side function", y);
+
+
