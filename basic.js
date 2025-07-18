2f323d04ffa1c1cdc22029e358cf1706e4ebefcf
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-19 00:39:59 +0600

    Add basic function examples to basic.js

--- basic.js
+++ basic.js
@@ -1,7 +1,15 @@
 //basic function
 
 
-function printName(){
-      console.log('bakir');
-}
-printName();
+// function printName(name){
+//       console.log("My name is ", name);
+// }
+// printName("Bakir");
+// printName("Bhuyain");
+/* parameter j sequince e rakhbo same sequince e arguiment ow rakhbo */
+// function printDetails(name,age,address){
+//       console.log(`My name is ${name}. My age is ${age}. My address is ${address}`);
+// }
+// printDetails("Bakir", "25", "Cumilla");
+
+
